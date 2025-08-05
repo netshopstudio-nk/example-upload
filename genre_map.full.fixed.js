ada28ed3e3f5df9ec3b2ee29351e89a898678430
netshopstudio-nk <nomurakana.15372525@gmail.com>
2025-08-05 21:47:29 +0900

    Add files via upload

---
+++ genre_map.full.fixed.js
@@ -0,0 +1,13 @@
+
+var genreMap = {
+  "食品>チーズ・乳製品>チーズ": "cheese",
+  "食品>精肉・肉加工品>牛肉": "meat_beef",
+  "ダイエット・健康>サプリメント>ビタミン": "supplement_vitamins",
+  "水・ソフトドリンク>お茶・紅茶>お茶飲料": "tea_drinks",
+  "日用品雑貨・文房具・手芸>タオル": "towel",
+  "水・ソフトドリンク>お酢飲料": "vinegar_drinks",
+  "食品>惣菜": "prepared_foods",
+  "食品>野菜・きのこ": "vegetables_mushrooms",
+  "ダイエット・健康>健康食品>ファイトケミカル": "nan"
+  // 必要に応じてさらに追加可能
+};
